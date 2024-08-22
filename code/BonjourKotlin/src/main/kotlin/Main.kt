11011e54e354883b96c333a1cf991ccd06aa9f14
Prada Velasque Waldomar <1635358@cegepmontpetit.ca>
2024-08-22 12:04:55 -0400

    Exercice Bonjour Kotlin
    mon nom et celui du prof

---
+++ code/BonjourKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+package org.pradavelasque
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val monNom = "pradavelasque"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Hello, " + monNom + "!")
+
+    val prenomProf = "P.O."
+    println("Hello, " + prenomProf + "!")
+    }
+
