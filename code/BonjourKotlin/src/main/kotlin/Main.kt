a13a6ebbe56524823d3425bf3ab69a4652a4486c
Prada Velasque Waldomar <1635358@cegepmontpetit.ca>
2024-08-26 21:15:45 -0400

    Exercice C# à Kotlin
    Changer le code c# en Kotlin

--- code/BonjourKotlin/src/main/kotlin/Main.kt
+++ code/BonjourKotlin/src/main/kotlin/Main.kt
@@ -3,15 +3,6 @@ package org.pradavelasque
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-fun main(args: Array<String>) {
-    if (args.isNotEmpty()) {
-        val nom = args[0]
-        afficher(nom)
-    } else {
-        println("*")
-    }
-}
+fun main() {
 
-fun afficher(nom: String) {
-    println("Bonjour $nom!")
 }
