fe98d66840c3f6410677b91da94ddb8d7f07884e
Waldomar Prada Velasque <1635358@cegepmontpetit.ca>
2024-08-27 10:22:35 -0400

    Exercice UnUn
    En progression

---
+++ code/BonjourKotlin/src/main/kotlin/UnUn.kt
@@ -0,0 +1,15 @@
+package org.pradavelasque
+
+fun main() {
+
+    val nombre: String = unUn("111213")
+
+    for (i in nombre){
+        println(i)
+    }
+
+}
+fun unUn(source: String): String{
+
+    return source
+}
