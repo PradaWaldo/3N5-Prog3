c59146e9eefecc141e018f5a2a3dba51621200ac
Prada Velasque Waldomar <1635358@cegepmontpetit.ca>
2024-08-26 22:02:13 -0400

    Exercice Triangles
    5 triangles de suite en utilisant une fonction Kotlin qui:
    prend la hauteur en paramètre
    renvoie une String avec le triangle

--- code/BonjourKotlin/src/main/kotlin/Triangle.kt
+++ code/BonjourKotlin/src/main/kotlin/Triangle.kt
@@ -3,7 +3,6 @@ package org.pradavelasque
 fun main(){
     println("Donner la hauteur du triangle")
     val hauteurTriangle: Int = readLine()!!.toInt()
-    val triangle: String = "*"
 
     for (i in 1 .. hauteurTriangle ){
         println("*".repeat(i))
--- code/BonjourKotlin/src/main/kotlin/Triangles.kt
+++ code/BonjourKotlin/src/main/kotlin/Triangles.kt
@@ -1,29 +1,22 @@
 package org.pradavelasque
 
-fun triangle(hauteur : Int) : String {
-    val sb = StringBuilder()
+fun main() {
+    println("Donner la hauteur du triangle")
+    val triangle: Int = readLine()!!.toInt()
 
-    // Construction du triangle ligne par ligne
-    for (i in 1..hauteur) {
-        // Ajouter les espaces avant les astérisques
-        sb.append(" ".repeat(hauteur - i))
-        // Ajouter les astérisques pour la ligne actuelle
-        sb.append("*".repeat(i * 2 - 1))
-        // Ajouter un saut de ligne après chaque ligne du triangle
-        sb.append("\n")
-    }
-
-    // Retourner le triangle construit sous forme de String
-    return sb.toString()
+    println(triangles(triangle))
+    println(triangles(triangle))
+    println(triangles(triangle))
+    println(triangles(triangle))
+    println(triangles(triangle))
 }
 
+fun triangles(hauteur: Int) : String {
 
-fun main() {
-    val hauteurs = listOf(3, 5, 7, 4, 6)
-    hauteurs.forEachIndexed { index, hauteur ->
-        println("Triangle ${index + 1} (hauteur $hauteur):")
-        println(triangle(hauteur))
-        println()
 
+    var nbrT = ""
+    for (i in 1 .. hauteur ) {
+         nbrT += "*".repeat(i)+ "\n"
     }
+    return nbrT
 }
