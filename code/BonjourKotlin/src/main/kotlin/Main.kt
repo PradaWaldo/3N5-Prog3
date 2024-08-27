f2390fa1cc4d6fc7abd12207ffb677c99bde17b4
Prada Velasque Waldomar <1635358@cegepmontpetit.ca>
2024-08-26 21:33:10 -0400

    Exercice Triangle
    reproduit un triangle en prenant sa hauteur par un argument de ligne de commande.

--- code/BonjourKotlin/src/main/kotlin/Main.kt
+++ code/BonjourKotlin/src/main/kotlin/Main.kt
@@ -3,6 +3,15 @@ package org.pradavelasque
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-fun main() {
+fun main(args: Array<String>) {
+    if (args.isNotEmpty()) {
+        val nom = args[0]
+        affichergf(nom)
+    } else {
+        println("*")
+    }
+}
 
+fun affichergf(nom: String) {
+    println("Bonjour $nom!")
 }
--- code/BonjourKotlin/src/main/kotlin/Triangle.kt
+++ code/BonjourKotlin/src/main/kotlin/Triangle.kt
@@ -1,15 +1,12 @@
 package org.pradavelasque
 
 fun main(){
-    val text = """
-    
-    * 
-    **
-    ***
-    ****
-""".trimIndent()
+    println("Donner la hauteur du triangle")
+    val hauteurTriangle: Int = readLine()!!.toInt()
+    val triangle: String = "*"
 
-    println(text)
-
+    for (i in 1 .. hauteurTriangle ){
+        println("*".repeat(i))
+    }
 
 }
