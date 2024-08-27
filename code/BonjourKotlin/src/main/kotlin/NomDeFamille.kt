34ccd0ab5b8dba4cab145ca509c6c54188e3509d
Prada Velasque Waldomar <1635358@cegepmontpetit.ca>
2024-08-26 21:06:19 -0400

    Exercice Bonjour Kotlin
    Creation de 2 fichiers

---
+++ code/BonjourKotlin/src/main/kotlin/NomDeFamille.kt
@@ -0,0 +1,6 @@
+package org.pradavelasque
+
+fun main() {
+    val nom = "Prada Velasque"
+    println("Bonjour, $nom")
+}
---
+++ code/BonjourKotlin/src/main/kotlin/PrenomDuProf.kt
@@ -0,0 +1,6 @@
+package org.pradavelasque
+
+fun main() {
+    val prenomDuProf = "Pierre-Olivier"
+    println("Salut, $prenomDuProf")
+}
