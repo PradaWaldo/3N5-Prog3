fdbe8b4085433b958739b23743892dba58cc72ed
Waldomar Prada Velasque <1635358@cegepmontpetit.ca>
2024-08-29 11:22:29 -0400

    Création du projet Types
    Exercice de compréhension sur les types

---
+++ code/Types/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+package org.pradavelasque
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val a : Int = 1                                   // type est		Int
+    val aa : Long = 1L                                 // type est		Long
+    val aaa : Long = a * 25 + aa * 100                 // type est		Int
+    val b : Double = 1.0                                 // type est 	Double
+    val c : String = "1"                                 // type est 	String
+    val d : Boolean = true                                // type est		Boolean
+    val laChose : String = c + b                         // type est      Long
+    val k : List<String> = listOf("1", "2", "3")               // type est      String
+    val l : List<Boolean> = mutableListOf(true, false, true)    // type est         Boolean
+}
