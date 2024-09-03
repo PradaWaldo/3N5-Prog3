85c696be95ad899999d93534033412043a74eb7d
Waldomar Prada Velasque <1635358@cegepmontpetit.ca>
2024-09-03 10:30:38 -0400

    Progres Exercice Moyenne
    Définis une autre fonction qui calcule la somme, la moyenne, le min et le max d'un tableau d'entiers et les affiche dans la console.

---
+++ code/Types/src/main/kotlin/Moyenne.kt
@@ -0,0 +1,14 @@
+package org.pradavelasque
+
+import kotlin.math.sign
+
+fun main(args : Array<String>){
+
+    var text : Int = args[0].toInt()
+}
+
+private fun creeTableauAleatoire(n: Int): Array<Int>{
+    var numeroAleatoire : Int =
+
+    return n
+}
