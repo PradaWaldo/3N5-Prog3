fd7336e27070df12891042218a66db4351c14561
Waldomar Prada Velasque <1635358@cegepmontpetit.ca>
2024-09-03 09:12:11 -0400

    FCT Exercice Pyramide
    Creation d'une pyramide

--- code/Types/src/main/kotlin/Pyramide.kt
+++ code/Types/src/main/kotlin/Pyramide.kt
@@ -5,12 +5,27 @@ fun main(args : Array<String>){
     var hauteurPyramide: Int = args[0].toInt()
     var etoile: String = "*"
     var espace : String =  " "
+    var nbrEtoile : Int = 1
+    var nbrEspace : Int = hauteurPyramide
 
-    for (i in 0..hauteurPyramide){
+    pyramid(hauteurPyramide, espace, nbrEspace, etoile, nbrEtoile)
 
-        println(espace.repeat(hauteurPyramide) + etoile)
-        etoile += etoile.repeat(2)
-        --hauteurPyramide
+}
+
+private fun pyramid(
+    hauteurPyramide: Int,
+    espace: String,
+    nbrEspace: Int,
+    etoile: String,
+    nbrEtoile: Int
+) {
+    var nbrEspace1 = nbrEspace
+    var nbrEtoile1 = nbrEtoile
+    for (i in 0..hauteurPyramide) {
+
+        println(espace.repeat(nbrEspace1) + etoile.repeat(nbrEtoile1))
+        nbrEtoile1 += 2
+        nbrEspace1 -= 1
+
     }
-
 }
