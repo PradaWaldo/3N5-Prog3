3ae8505c22d18fb8913629081a279c0e20ed21c5
Prada Velasque Waldomar <1635358@cegepmontpetit.ca>
2024-09-11 21:53:29 -0400

    FCT Exercice EcrireFichier
    Ecrire dans un fichier

--- code/librairieStandard/src/main/kotlin/EcrireFichier.kt
+++ code/librairieStandard/src/main/kotlin/EcrireFichier.kt
@@ -3,19 +3,32 @@ package org.pradavelasque
 import java.io.File
 
 fun main(args: Array<String>){
-    var nomFichier = args[0]
+
+    var nomFichier : File = File(args[0])
     var nomPersonne = args[1]
     var prenomPersonne = args[2]
 
-    var fichier : File = File(nomFichier)
 
-    if (fichier.exists()){
-        var textRead: String = fichier.readText()
-        var textFichier: List<String> = textRead.split("\n")
-        var newTexte1: String = textFichier.joinToString("\n---------------------\n")
 
-        fichier.writeText(newTexte1)
-        println(textRead)
+    if (nomFichier.exists()){
+        try {
+            var textFile : String = nomFichier.readText()
+            println(textFile)
+            var listText : List<String> = textFile.split("\n")
+            println(listText)
+
+            var newText : String = listText.joinToString("-------------")
+            println(newText)
+
+            var newFile : File = File("PradaWaldomar.txt")
+            newFile.writeText("Mon nom est $nomPersonne $prenomPersonne")
+
+        } catch (e: Exception) {
+            println("Le fichier $nomFichier ne peut pas etre lu")
+        }
+    }
+    else{
+        println("Le fichier $nomFichier n'existe pas")
     }
 
 
