fd22bfd173c3c101c308bcbd0851fcfb78e5e094
Prada Velasque Waldomar <1635358@cegepmontpetit.ca>
2024-10-29 21:45:46 -0400

    Progress OrdreAlpha
    Affichage des prenom et des boutons

---
+++ code/OrdreAlpha/app/src/main/java/org/pradavelasque/MainActivity.kt
@@ -0,0 +1,48 @@
+package org.pradavelasque
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import org.pradavelasque.Adapters.MonAdapter
+import org.pradavelasque.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: MonAdapter // L'adapteur est accessible partout dans notre classe
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupRecycler()
+        fillRecycler()
+    }
+
+    private fun setupRecycler() {
+        adapter = MonAdapter() // Créer l'adapteur
+        binding.rvMonAdapter.adapter = adapter // Assigner l'adapteur au RecyclerView
+        binding.rvMonAdapter.setHasFixedSize(true) // Option pour améliorer les performances
+        binding.rvMonAdapter.addItemDecoration( // Ajouter un séparateur entre chaque élément
+            DividerItemDecoration(
+                binding.rvMonAdapter.context, DividerItemDecoration.VERTICAL
+            )
+        )
+    }
+
+    private fun fillRecycler() {
+        val items: MutableList<String> = mutableListOf()
+        for (i in 1..10000) {
+            items.add("Item #$i")
+        }
+        adapter.submitList(mutableListOf(
+                "Lucas","Emma","Léa","Hugo","Chloé","Maxime","Clara","Nathan",
+        "Sarah","Jules","Zoé","Louis","Inès","Gabriel","Camille","Mathis",
+        "Alice","Paul","Manon","Enzo","Charlotte","Raphaël","Jade","Tom","Anaïs")) // Pour changer le contenu de la liste, utiliser submitList de l'adapteur
+    }
+
+}
