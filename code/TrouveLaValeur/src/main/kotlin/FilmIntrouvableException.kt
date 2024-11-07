5a4be5ff64f7a30ae1630cc5910bc7ad9abd3326
Waldomar Prada Velasque <1635358@cegepmontpetit.ca>
2024-11-07 12:33:36 -0500

    FCT Exercice TrouveLaValeur
    Un appel normal (où on trouve l'élément).
    Un appel exceptionnel (qui lance l'exception). Vous devez faire un try catch pour attraper l'exception.

--- code/TrouveLaValeur/src/main/kotlin/FilmIntrouvableException.kt
+++ code/TrouveLaValeur/src/main/kotlin/FilmIntrouvableException.kt
@@ -2,5 +2,5 @@ package org.pradavelasque
 
 class FilmIntrouvableException : Exception() {
     override val message: String?
-        get() = "Le film est introuvable"
+        get() = "Le film est introuvable dans notre magasin"
 }
--- code/TrouveLaValeur/src/main/kotlin/Main.kt
+++ code/TrouveLaValeur/src/main/kotlin/Main.kt
@@ -9,16 +9,19 @@ fun trouveru(valeur : Array<String>){
     var films : Array<String> = arrayOf("Hobbit", "Father", "Venom", "Joker","Batman", "Watchman");
 
     //Chercher la valeur dans le tableau movies
-    for (film in valeur){
-        if (films.contains(film)){
-            val index = films.indexOf(film)
+    try {
+        for (film in valeur){
+            if (films.contains(film)){
+                val index = films.indexOf(film)
 
-            println("Nous avons trouvé votre film $film son numéro est $index")
-        }
-        else{
-            println("Votre film est introuvable")
+                println("Nous avons trouvé votre film $film son numéro est $index")
+            }
+            else{
+                throw FilmIntrouvableException()
+            }
         }
     }
-
-
+    catch (e: FilmIntrouvableException) {
+        println(e.message)
+    }
 }
